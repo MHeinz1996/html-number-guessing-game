68cad9309959ca8c135f5e63b7535f0168144c63
MHeinz1996 <michael.heinzinger@gmail.com>
2022-06-15 16:36:45 -0500

    Completed program

--- app.js
+++ app.js
@@ -1,2 +1,28 @@
-console.log("HELLO PAPA PLATOON!")
-// Your function(s) should go here that will interact with the webpage or DOM
+function GuessingGame() {
+    
+    last_guess = document.getElementById("user-input")
+    let output = ''
+    
+    if(+last_guess.value > num) {
+        output+='High'
+    } else if(+last_guess.value < num) {
+        output+='Low'
+    } else if(+last_guess.value === num) {
+        output+='Correct!'
+    } else {
+        output+="Please enter a number."
+    }
+
+    return output
+}
+
+function onClickHandler() {
+    output = GuessingGame();
+    console.log("ran guessing game")
+    document.getElementById('result').innerHTML=output
+}
+
+let last_guess = null
+let result = null
+let num = Math.ceil(Math.random() * 100);
+console.log(num)
