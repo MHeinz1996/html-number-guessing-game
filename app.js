6220b1da743e013faa279a2e9ef2c004a3f8062d
MHeinz1996 <michael.heinzinger@gmail.com>
2022-06-15 21:16:26 -0500

    Completed Project

--- app.js
+++ app.js
@@ -17,12 +17,28 @@ function GuessingGame() {
 }
 
 function onClickHandler() {
+    if(count > 0) {punct = ','}
     output = GuessingGame();
     console.log("ran guessing game")
     document.getElementById('result').innerHTML=output
+    if(output === 'Correct!') {
+        punct = '!'
+        document.querySelector('body').style.background = "green"
+        document.getElementById('guesses').innerHTML+=`, ${last_guess.value}${punct}`
+    } else if(last_guess.value >= num+10 || last_guess.value <= num-10) {
+        document.querySelector('body').style.background = "red"
+        document.getElementById('guesses').innerHTML+=`${punct} ${last_guess.value}`
+    } else {
+        document.querySelector('body').style.background = "yellow"
+        document.getElementById('guesses').innerHTML+=`${punct} ${last_guess.value}`
+    }
+    
+    count++
 }
 
 let last_guess = null
 let result = null
+let count = 0
+let punct = ''
 let num = Math.ceil(Math.random() * 100);
 console.log(num)
